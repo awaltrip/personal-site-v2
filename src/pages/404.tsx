57548abc8f3e326ec76f946ccd894787c20c7826
anna waltrip <annawaltrip@gmail.com>
2024-01-03 00:21:41 -0500

    Refactor all styles from 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,28 +1,7 @@
 import * as React from 'react'
 import { Link, HeadFC, PageProps } from 'gatsby'
 import { Layout } from '@components'
-
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
+import * as globalStyles from '@styles/global.module.scss'
 
 const NotFoundPage: React.FC<PageProps> = () => {
   return (
@@ -33,7 +12,7 @@ const NotFoundPage: React.FC<PageProps> = () => {
         {process.env.NODE_ENV === "development" ? (
           <>
             <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
+            Try creating a page in <code className={globalStyles.code}>src/pages/</code>.
             <br />
           </>
         ) : null}
