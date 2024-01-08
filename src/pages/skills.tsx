5b72df1087b1e06fb7b7464d313e34e195da3929
anna waltrip <annawaltrip@gmail.com>
2024-01-08 18:37:07 -0500

    Create dropdown element

--- src/pages/skills.tsx
+++ src/pages/skills.tsx
@@ -1,12 +1,23 @@
 import * as React from 'react';
 import type { HeadFC, PageProps } from 'gatsby';
 import { Layout } from '@components';
+import * as globalStyles from '@styles/global.module.scss';
 
 const SkillsPage: React.FC<PageProps> = () => {
   return (
     <Layout heading="Skills">
-      <p>Here is the full list of my skills.</p>
-      <p>Sort by: (Alphabetical | Proficiency level)</p>
+      <p>Here is the full list of my technical skills.</p>
+      <p>
+        Sort by: 
+        <select onChange={() => console.log('dropdown onChange') }>
+          <option key="alphabetical" value="Alphabetical">
+            Alphabetical
+          </option>
+          <option key="proficiencyLevel" value="Proficiency level">
+            Proficiency level
+          </option>
+        </select>
+      </p>
       <p>(Coming soon)</p>
     </Layout>
   );
