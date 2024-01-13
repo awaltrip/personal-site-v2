501f4c8c2dca85d8185bb0039e6659fc59c81bf6
anna waltrip <annawaltrip@gmail.com>
2024-01-13 00:46:30 -0500

    Create and style skills grid

--- src/pages/skills.tsx
+++ src/pages/skills.tsx
@@ -1,7 +1,48 @@
 import * as React from 'react';
 import type { HeadFC, PageProps } from 'gatsby';
 import { Layout, Seo } from '@components';
+import * as Logos from '@images/logos'
 import * as globalStyles from '@styles/global.module.scss';
+
+interface Skill {
+  name: string;
+  image?: React.ReactElement;
+  proficiencyLevel?: number;
+  url?: string;
+}
+
+const skills: Skill[] = [
+  {
+    name: 'Angular',
+    image: <Logos.Angular/>,
+    url: 'https://angular.io/'
+  },
+  {
+    name: 'AWS Lambda',
+    image: <Logos.Angular/>,
+    url: 'https://aws.amazon.com/'
+  },
+  {
+    name: 'AWS S3',
+    image: <Logos.Angular/>,
+    url: 'https://aws.amazon.com/'
+  },
+  {
+    name: 'Docker',
+    image: <Logos.Angular/>,
+    url: 'https://www.docker.com/'
+  },
+  {
+    name: 'Gatsby',
+    image: <Logos.Angular/>,
+    url: 'https://www.gatsbyjs.com/'
+  },
+  {
+    name: 'Java',
+    image: <Logos.Angular/>,
+    url: 'https://spring.io/projects/spring-boot/'
+  },
+];
 
 const SkillsPage: React.FC<PageProps> = () => {
   return (
@@ -18,7 +59,18 @@ const SkillsPage: React.FC<PageProps> = () => {
           </option>
         </select>
       </p>
-      <p>(Coming soon)</p>
+      <div className={globalStyles.skillsGrid}>
+        {skills.map((skill) =>
+          <a className={globalStyles.skillBox} href={skill.url} target="_blank">
+            <div className={globalStyles.skillBoxImg}>
+              {skill.image}
+            </div>
+            <span className={globalStyles.skillBoxLink}>
+              {skill.name}
+            </span>
+          </a>
+        )}
+      </div>
     </Layout>
   );
 };
