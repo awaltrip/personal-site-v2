9087fd35e6ecb552e644bd12a7e946d554a2295c
anna waltrip <annawaltrip@gmail.com>
2024-01-16 15:21:32 -0500

    Update home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,16 @@ const IndexPage: React.FC<PageProps> = () => {
   return (
     <Layout heading="Hi, my name is" headingAccent="Anna Waltrip">
       <p>I'm a full-stack software engineer specializing in building exceptional user experiences on the web.</p>
-      <p>This site is currently under construction, but you can check out the navigation links above.</p>
+      <p>This site is currently a work in progress, but you can check out the navigation links above.</p>
+      <p>Features coming soon:
+        <ul>
+          <li>Projects page</li>
+          <li>Contact form</li>
+          <li>Sort feature on skills page</li>
+          <li>Dark / light themes</li>
+          <li>Mobile view</li>
+        </ul>
+      </p>
     </Layout>
   );
 };
