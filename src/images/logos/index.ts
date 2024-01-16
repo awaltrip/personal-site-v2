16d69ce2cb989faf188f138fc658429ce7697dd5
anna waltrip <annawaltrip@gmail.com>
2024-01-16 14:46:20 -0500

    Add more skill logos

--- src/images/logos/index.ts
+++ src/images/logos/index.ts
@@ -1,1 +1,6 @@
 export { default as Angular } from './angular.svg';
+export { default as AWSLambda } from './aws-lambda.svg';
+export { default as AWSS3 } from './aws-s3.svg';
+export { default as Docker } from './docker.svg';
+export { default as Gatsby } from './gatsby.svg';
+export { default as Java } from './java.svg';
