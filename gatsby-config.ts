6bbcef3d7d7607734b3bb8ad373e5998dc426ac0
anna waltrip <annawaltrip@gmail.com>
2024-01-16 16:53:19 -0500

    Update siteUrl

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -4,7 +4,7 @@ const path = require('path');
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    siteUrl: `https://awaltrip.netlify.app`,
+    siteUrl: `https://annawaltrip.com`,
     title: `Anna Waltrip`,
     description: `Portfolio website of Anna Waltrip, full stack software engineer. A summary of professional skills, 
       project details, and contact information.`,
