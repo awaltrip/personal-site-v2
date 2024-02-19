dfe0ea0ef456d4dbbf674b2309395353fe4bdaf0
anna waltrip <annawaltrip@gmail.com>
2024-02-19 11:24:08 -0500

    Configure file source

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -17,6 +17,13 @@ const config: GatsbyConfig = {
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+         name: 'images',
+         path: `${__dirname}/src/images/`
+      }
+    },
+    {
       resolve: `gatsby-plugin-alias-imports`,
       options: {
         alias: {
