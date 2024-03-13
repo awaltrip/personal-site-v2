9442bd6da2c612766262fdc94a960a7de667ec50
anna waltrip <annawaltrip@gmail.com>
2024-03-13 12:51:52 -0400

    Try fix for initial icon incorrect SSR

--- src/components/themeToggle.tsx
+++ src/components/themeToggle.tsx
@@ -5,8 +5,9 @@ import * as styles from '@styles/main.module.scss';
 const ThemeToggle: React.FC = () => {
 
   const { theme, updateTheme } = useTheme();
-  const [icon, setIcon] = React.useState(theme === 'light' ? 'moon' : 'sun');
+  const [icon, setIcon] = React.useState('');
 
+  React.useEffect(() => setIcon(theme === 'light' ? 'moon' : 'sun'), []);
   React.useEffect(() => setIcon(theme === 'light' ? 'moon' : 'sun'), [theme]);
 
   const changeTheme = () => updateTheme(theme === 'light' ? 'dark' : 'light');
