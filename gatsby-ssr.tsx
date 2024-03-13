966cf5238d4330c9dc2e2e4499c48175bf185519
anna waltrip <annawaltrip@gmail.com>
2024-03-13 12:26:59 -0400

    Suppress VSCode complaints

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+// @ts-ignore // ignore IDE error "Cannot find module '@context/themeContext'"
 import { ThemeProvider } from '@context/themeContext';
 
 export const wrapRootElement = ({ element }) => (
